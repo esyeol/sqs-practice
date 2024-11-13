05e4db49afab906ee0450a283de8060b97318ef0
Evelops <co6747@naver.com>
2024-11-13 17:08:07 +0900

    Feat:  Nack 모듈 추가, Consumer가 이벤트 수신에 실패시 Nack Message를 보내도록 핸들링 로직 추가

--- consumer/src/sqs/helpers/delete.message.ts
+++ consumer/src/sqs/helpers/delete.message.ts
@@ -1,9 +1,9 @@
 import 'dotenv/config';
-import { DeleteMessageCommand, Message } from "@aws-sdk/client-sqs";
+import { ChangeMessageVisibilityCommand, DeleteMessageCommand, Message } from "@aws-sdk/client-sqs";
 
 /**
  * Consumer에서 이벤트를 Listen 하고 내용 처리후(DB작업, RPC, PUSH, Batch..) 
- * 다시 SQS로 성공 완료 메시지를 전송해서 대기열 제거
+ * 다시 SQS로 성공 완료 메시지를 전송해서 대기열 제거 -> ACK 
  * 
  * @param message sqs message info
 */
@@ -12,5 +12,18 @@ export const sqsDeleteResponseMessage = async(message: Message): Promise<DeleteM
     QueueUrl: process.env.SQS_URL,
     ReceiptHandle: message.ReceiptHandle,
   });
+}
+
+/**
+ * Consumer에서 이벤트 수신에 실패했을 경우 NACK 메시지를 queue로 이벤트 발송
+ * 실패 회신 Event -> NACK
+*/
+export const sqsFailedResponseMessage = async(message: Message): Promise<ChangeMessageVisibilityCommand> => {
+  return new ChangeMessageVisibilityCommand({
+    QueueUrl: process.env.SQS_URL,
+    ReceiptHandle: message.ReceiptHandle,
+    VisibilityTimeout: 60, // deafult 30초  
+  });
+  
 }
 
--- consumer/src/sqs/sqs.consumer.ts
+++ consumer/src/sqs/sqs.consumer.ts
@@ -1,30 +1,40 @@
 import 'dotenv/config';
 import { ReceiveMessageCommand } from "@aws-sdk/client-sqs"
 import { sqsClient } from "./sqs.module";
-import { sqsDeleteResponseMessage } from './helpers/delete.message';
+import { sqsDeleteResponseMessage, sqsFailedResponseMessage } from './helpers/delete.message';
+
+const command = new ReceiveMessageCommand({
+  QueueUrl: process.env.SQS_URL,
+  MaxNumberOfMessages: 10,
+  MessageAttributeNames: ["unique"],
+  WaitTimeSeconds: 10, 
+});
 
 export const sqsConsumer = async() => {
-  const command = new ReceiveMessageCommand({
-    QueueUrl: process.env.SQS_URL,
-    MaxNumberOfMessages: 10,
-    MessageAttributeNames: ["unique"],
-    WaitTimeSeconds: 0, 
-  });
+  // polling 
+  while(true) {
+    // SQS 이벤트 수신 
+    const response = await sqsClient.send(command);
+    try {
+      /**
+      * Consumer에서 이벤트를 전달 받았을 경우 핸들링 작업(DB, PUSH, Batch..)
+      * 이후 표준 대기열에서 이벤트 제거(ACK)
+      * */
+      if(response.Messages?.length){
+        console.log('success');
+        const message = response.Messages[0];
+        const deleteCompletedMessage = await sqsDeleteResponseMessage(message);
+        // sqs 대기열 삭제 
+        await sqsClient.send(deleteCompletedMessage);
+      } 
+    } catch (error) {
+      // Nack Message Basic SQS Failed 
+      console.error('SQS Conumser RecieveMessage Failed -->', error);
+      // TODO:// Refactor
+      const message = response.Messages[0];
+      const deleteFailedCompletedMessage = await sqsFailedResponseMessage(message);
 
-  const response = await sqsClient.send(command);
-  console.log('response ->', response.Messages);
-  
-  /**
-   * Consumer에서 이벤트를 전달 받았을 경우 핸들링 작업(DB, PUSH, Batch..)
-   * 이후 표준 대기열에서 이벤트 제거 
-   * */ 
-  
-  if(response.Messages?.length){
-    const message = response.Messages[0];
-    const deleteCompletedMessage = await sqsDeleteResponseMessage(message);
-    // sqs 대기열 삭제 
-    await sqsClient.send(deleteCompletedMessage);
+      await sqsClient.send(deleteFailedCompletedMessage);
+    }
   }
-
-  
 }
